623519afaea2217758aeb5ae199a40221721fea4
2088959m <2088959m@student.gla.ac.uk>
2015-01-31 02:27:44 +0000

    Chapter 9 not completed

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Page, Category, UserProfile
+
 
 
 class CategoryForm(forms.ModelForm):
@@ -30,8 +32,8 @@ class PageForm(forms.ModelForm):
         # Here, we are hiding the foreign key.
         # we can either exclude the category field from the form,
         exclude = ('category',)
-        #or specify the fields to include (i.e. not include the category field)
-        #fields = ('title', 'url', 'views')
+        # or specify the fields to include (i.e. not include the category field)
+        # fields = ('title', 'url', 'views')
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -43,3 +45,16 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
 
         return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
