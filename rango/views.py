af75e2a45d92727a386d8c2e1bbf4e42ed8b7a90
2088959m <2088959m@student.gla.ac.uk>
2015-01-31 01:46:06 +0000

    Chapter 8 - completed

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 
 def index(request):
@@ -28,6 +29,7 @@ def category(request, category_name_slug):
 
         context_dict['pages'] = pages
         context_dict['category'] = category
+        context_dict['category_name_slug'] = category_name_slug
 
     except Category.DoesNotExist:
         pass
@@ -58,3 +60,30 @@ def add_category(request):
     # Bad form (or form details), no form supplied...
     # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+                cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context_dict = {'form':form, 'category': cat, 'category_name_slug': category_name_slug}
+
+    return render(request, 'rango/add_page.html', context_dict)
